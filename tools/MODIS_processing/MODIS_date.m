14140d806adb7294b04302303d715ab978816414
M. J. Brodzik <brodzik@nsidc.org>
2019-06-17 13:59:53 -0600

    Deleting MODIS_processing scripts.
    These scripts are duplicated in HKHK_scripts repo, so we are
    keeping them there and deleting them from CCHF repo.

--- tools/MODIS_processing/MODIS_date.m
+++
@@ -1,8 +0,0 @@
-function dateVec = MODIS_date(fileNm)
-
-%Dates are between the 2nd and 3rd periods:
-indPer = regexpi(fileNm, '\.');
-
-dateVec = [...
-    str2double(fileNm(indPer(1)+2:indPer(1)+5)), ...
-    str2double(fileNm(indPer(2)-3:indPer(2)-1))];
